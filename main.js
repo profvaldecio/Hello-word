d39092b467a7dd1f6629115cfe057ed0dc3f457e
profvaldecio <164545303+profvaldecio@users.noreply.github.com>
2024-03-22 19:55:41 -0300

    Construção do main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuário = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuário =="" ){
+   nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if (nomeUsuário == null) {
+    elemento.textContent = "Seja Bem-Vindo"
+}
+else{
+    elemento.textContent = nomeUsuário;
+}
